dfd404135da2a3ac4ebb8a72cebd78e6a8e900be
sbabybird <sbabybird@gmail.com>
2014-11-04 14:10:01 +0800

    add atoi.cpp

---
+++ atoi.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+  public:
+    int atoi(const char *str) {
+      int r = 0;
+      bool b = false;
+      while(str!=NULL && *str !='\0') {
+        char p = *str++;
+        if (p == '-') {
+          b = true;
+          continue;
+        }
+        else if (p == '+') {
+          continue;
+        }
+        else {
+          r = r*10;
+          r += p-48;
+        }
+      }
+      return b ? r*-1 : r;
+    }
+};
+
+int main(void) {
+  char *str = new char[256];
+  cin >> str;
+  Solution s;
+  cout << s.atoi(str) << endl;
+  delete str;
+  str = NULL;
+  return 0;
+}
